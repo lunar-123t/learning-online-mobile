99a0333116edb67b9f0c98841d133fee454050f0
huyle55 <ledinhngochuy55@gmail.com>
2022-10-29 17:01:11 +0700

    huy

--- screens/VideoScreen.tsx
+++ screens/VideoScreen.tsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import React, { useState, useCallback, useRef, useEffect } from "react";
-import { Button, View, StyleSheet, Alert, Text, Image, FlatList, TouchableOpacity, AsyncStorage } from "react-native";
-=======
-
 import React, { useState, useCallback, useRef, useEffect } from "react";
 import { Button, View,StyleSheet, Alert, Text, Image ,FlatList, TouchableOpacity, AsyncStorage } from "react-native";
->>>>>>> 857dd7007462f483731d8d95f40f47c08051f9f9
 import YoutubePlayer from "react-native-youtube-iframe";
-
 import Icon from "react-native-vector-icons/Ionicons"
 import { env } from "../env";
 import axios from "axios";
@@ -18,69 +11,12 @@ const config = {
   }
 };
 const { baseUrl } = env();
-<<<<<<< HEAD
-=======
-const Danhsach=[
-      {
-        anh: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKVIdoPWcjlGpJKQFOv9qOuC5eHKneQLn1Sg&usqp=CAU",
-        tenbai: 'Laravel-bai 1',
-        thoiluong: '3:00',
-        video_id: "O71GdeeND68"
-      },
-      {
-        anh: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKVIdoPWcjlGpJKQFOv9qOuC5eHKneQLn1Sg&usqp=CAU",
-        tenbai: 'Laravel-bai 2',
-        thoiluong: '3:00',
-        video_id: "dXBZEr9nh9w"
-      },
-      {
-        anh: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKVIdoPWcjlGpJKQFOv9qOuC5eHKneQLn1Sg&usqp=CAU",
-        tenbai: 'Laravel-bai 3',
-        thoiluong: '3:00',
-        video_id: "dV-znS6RPbQ"
-      },
-      {
-        anh: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKVIdoPWcjlGpJKQFOv9qOuC5eHKneQLn1Sg&usqp=CAU",
-        tenbai: 'Laravel-bai 4',
-        thoiluong: '3:00',
-        video_id: "dV-znS6RPbQ"
-      },
-      {
-        anh: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKVIdoPWcjlGpJKQFOv9qOuC5eHKneQLn1Sg&usqp=CAU",
-        tenbai: 'Laravel-bai 5',
-        thoiluong: '3:00',
-        video_id: "n6Pnzi6r9NU"
-      },
-      {
-        anh: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKVIdoPWcjlGpJKQFOv9qOuC5eHKneQLn1Sg&usqp=CAU",
-        tenbai: 'Laravel-bai 6',
-        thoiluong: '3:00',
-        video_id: "n6Pnzi6r9NU"
-      },
-      {
-        anh: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKVIdoPWcjlGpJKQFOv9qOuC5eHKneQLn1Sg&usqp=CAU",
-        tenbai: 'Laravel-bai 7',
-        thoiluong: '3:00'
-      },
-      {
-        anh: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKVIdoPWcjlGpJKQFOv9qOuC5eHKneQLn1Sg&usqp=CAU",
-        tenbai: 'Laravel-bai 8',
-        thoiluong: '3:00'
-      }
 
-
-]
->>>>>>> 857dd7007462f483731d8d95f40f47c08051f9f9
 
 export default function VideoScreen() {
   const [videoid, setVideoid] = useState("O71GdeeND68");
   const [playing, setPlaying] = useState(false);
   const [dsvideo, setDsvideo] = React.useState([]);
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> 857dd7007462f483731d8d95f40f47c08051f9f9
   const onStateChange = useCallback((state: string) => {
     if (state === "ended") {
       setPlaying(false);
@@ -97,14 +33,9 @@ export default function VideoScreen() {
     axios.get(`${baseUrl}khoahoc/listvideo`, config).then((response) => {
       const data = response.data;
       setDsvideo(data);
-<<<<<<< HEAD
+
     }).catch((e) => {
 
-=======
-    }).catch((e)=>{
-      
->>>>>>> 857dd7007462f483731d8d95f40f47c08051f9f9
-      console.log(e)
     });
   }, []);
 
@@ -115,31 +46,7 @@ export default function VideoScreen() {
       <TouchableOpacity style={{ flexDirection: 'row' }}
         onPress={() => {
 
-          setVideoid(item.video_id)
-<<<<<<< HEAD
-
-        }}
-      >
-        <View style={{ width: '50%', height: 50, margin: 1 }}>
-
-          <Image
-            style={{ width: '80%', height: 98 }}
-            source={{ uri: item.url_anh }}
-          />
-        </View>
-        <View style={{ right: 30 }}>
-          <Text>
-            {item.ten_bai}
-          </Text>
-          <Text>
-            <Icon name="time-outline" size={14} />
-            {item.thoi_luong}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    </View>
-=======
-          
+          setVideoid(item.video_id)       
       }} 
         >
           <View style={{width:'50%',height:50,margin:1}}>
@@ -160,7 +67,6 @@ export default function VideoScreen() {
           </View>
         </TouchableOpacity>    
     </View>       
->>>>>>> 857dd7007462f483731d8d95f40f47c08051f9f9
   );
 
 
